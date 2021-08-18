589132a82af341eee6d24be9b89fe6502ef111d9
wjsrlahrlco1998 <85821684+wjsrlahrlco1998@users.noreply.github.com>
2021-08-18 22:22:41 +0900

    Create FunctionQueue스케쥴링.c

---
+++ 기타 알고리즘/FunctionQueue스케쥴링.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+
+typedef void element;
+typedef struct QueueNode {
+	element (*data)();
+	struct QueueNode* link;
+} QueueNode;
+
+typedef struct {
+	QueueNode* front, * rear;
+} LinkedQueueType;
+
+//큐 초기화 함수
+void init(LinkedQueueType* q) {
+	q->front = q->rear = 0;
+}
+
+//공백 상태 검출 함수
+int is_empty(LinkedQueueType* q) {
+	return (q->front == NULL);
+}
+
+//포화 상태 검출 함수
+int is_full(LinkedQueueType* q) {
+	return 0;
+}
+
+//삽입 함수 (큐의 마지막에 함수 포인터를 추가)
+void enqueue(LinkedQueueType* q, element(*data)()) {
+	QueueNode* temp = (QueueNode*)malloc(sizeof(QueueNode));
+	temp->data = data;
+	temp->link = NULL;
+	if (is_empty(q)) {
+		q->front = temp;
+		q->rear = temp;
+	}
+	else {
+		q->rear->link = temp;
+		q->rear = temp;
+	}
+
+}
+
+//삭제 함수 (큐로부터 첫번째 포인터를 읽음)
+int dequeue(LinkedQueueType* q) {
+	QueueNode* temp = q->front;
+	element(*data)();
+	if (is_empty(q)) {
+		fprintf(stderr, "스택 공백 \n");
+		return NULL;
+	}
+	else {
+		data = temp->data;
+		q->front = q->front->link;
+		if (q->front == NULL)
+			q->rear = NULL;
+		free(temp);
+		return data;
+	}
+}
+
+//출력 함수
+void print_queue(LinkedQueueType* q) {
+	QueueNode* p;
+	for (p = q->front; p != NULL; p = p->link)
+		printf("%d->", (int)p->data);
+	printf("NULL\n");
+}
+
+//DeviceA 동작 함수
+void deviceA() {
+	printf("DeviceA의 동작\n"); //장치A에게 필요한 일을 처리
+}
+//DeviceB 동작 함수
+void deviceB() {
+	printf("DeviceB의 동작\n"); //장치B에게 필요한 일을 처리
+}
+//disable
+void disable() {
+	printf("인터럽트 금지 시작-----\n");
+}
+//enable
+void enable() {
+	printf("인터럽트 금지 해제-----\n");
+}
+
+//메인함수
+int main() {
+	LinkedQueueType queue;
+	init(&queue);
+	void(*fp)();
+
+	enqueue(&queue, deviceA); print_queue(&queue);
+	enqueue(&queue, deviceB); print_queue(&queue);
+
+	while (TRUE) {
+		while (!is_empty(&queue)) { //함수 포인터들의 큐가 비어 있다면 기다린다
+			disable();
+			fp = dequeue(&queue); //큐에 있는 첫번째 함수를 호출한다
+			fp();
+			enable();
+		}
+	}
+	
+}
